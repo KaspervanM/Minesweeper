9b25f44cb8c9cebe771f190df1a8590059a10cda
KaspervanM <kaspervanm@gmail.com>
2022-01-19 22:27:49 +0100

    Linting + sort of functional evolution

---
+++ ms_ev.py
@@ -0,0 +1,383 @@
+"""Trains AI to play minesweeper using an evolutionary algorithm
+"""
+import sys
+from random import randrange
+from ctypes import windll
+from operator import itemgetter
+from datetime import datetime
+from pygame.locals import Rect
+import pygame
+from interfaces.model_interface import (
+    Model,
+    feedforward,
+    freeModel,
+    getMutated,
+    save_model,
+)
+
+
+# Initialize models
+MODEL_COUNT = 5
+SURVIVOR_COUNT = int(MODEL_COUNT * 0.20)
+SAVE_REQUIREMENT_RATIO = 0.95
+GAME_SPACER = 5
+CYCLE_COUNTER = 0
+SEED = 0
+SHAPE = [81, 5, 1]
+ACT_FUNCTS = [4, 2]
+MUTATION_RATE = 0.5
+MUTATION_DEGREE = 0.25
+OBJECTS = []
+for i in range(MODEL_COUNT):
+    # boxes, blocksleft, checkedBoxes, fitness, gamecount, model, clickCounter
+    OBJECTS.append([[], 0, [], 0, 0, Model(
+        SEED + i, len(SHAPE), SHAPE, ACT_FUNCTS), 0])
+print(OBJECTS)
+
+#load_model(models[0][0], "saves/model1-2021-10-08_07-41-36.dat")
+
+NAME = f"_SEED{SEED}_s"
+for s in SHAPE:
+    NAME += str(s) + "-"
+NAME = NAME[:-1] + "_a"
+for a in ACT_FUNCTS:
+    NAME += str(a) + "-"
+NAME = NAME[:-1]
+fname = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + NAME
+
+with open("Data/" + fname + "_LOG.txt", "a+", encoding="utf8") as f:
+    f.write(
+        NAME
+        + "\n"
+        + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        + "\t"
+        + str(CYCLE_COUNTER)
+        + f"\tto save:  {int(SAVE_REQUIREMENT_RATIO)}, pool size = {MODEL_COUNT},\
+survivor count = {SURVIVOR_COUNT}, games per cycle = {GAME_SPACER},\
+mutation rate = {MUTATION_RATE}, mutation degree = {MUTATION_DEGREE}"
+    )
+
+
+def save(model, top=False):
+    filename_end = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if top:
+        filename_end += "_TOP"
+    save_model(model, "saves/model1-" + filename_end + ".dat")
+    print("Saved")
+
+# To do: void that changes the models in OBJECTS
+
+
+def get_mutations(survivors):
+    top_ = sorted(OBJECTS, key=itemgetter(3), reverse=True)
+    top = top_[:survivors]
+    freeModel([o[5] for o in top_[survivors:]])
+    #print("TOP: ", top)
+
+    print("Top scores: ", [t[3] for t in top])
+
+    with open("Data/" + fname + "_LOG.txt", "a+", encoding="utf8") as file:
+        file.write(
+            datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            + "\t"
+            + str(CYCLE_COUNTER)
+            + "\t["
+            + ",".join([str(t[1]) for t in top])
+            + "]\n"
+        )
+
+    if top[0][3] >= int(SAVE_REQUIREMENT_RATIO * (81 - 10)):
+        save(top[0][5], True)
+        sys.exit()
+
+    new_count = MODEL_COUNT - survivors
+    new_objects = []
+    #OBJECTS.append([[], 0, [], 0, 0, Model( SEED + i, len(SHAPE), SHAPE, ACT_FUNCTS), 0])
+    for top_index in range(survivors):
+        new_objects.append([[], 0, [], 0, 0, top[top_index][5], 0])
+        for new_obj_index in range(int(new_count / survivors + 0.5)):
+            new_objects.append(
+                [[], 0, [], 0, 0,
+                    Model(
+                        obj=getMutated(
+                            top[top_index][5],
+                            SEED + top_index + new_obj_index,
+                            MUTATION_RATE,
+                            MUTATION_DEGREE,
+                        )), 0
+                 ]
+            )
+    return new_objects[:MODEL_COUNT]
+
+
+def message_box(title, text, style):
+    return windll.user32.MessageBoxW(0, text, title, style)
+
+
+pygame.init()
+bomb = pygame.image.load("bomb.png")
+pygame.display.set_icon(bomb)
+surfObj = pygame.display.set_mode((173, 200))
+pygame.display.set_caption("Minesweeper")
+
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+
+blue = pygame.Color(0, 0, 255)
+white = pygame.Color(255, 255, 255)
+black = pygame.Color(0, 0, 0)
+
+font = pygame.font.Font("UniversLTStd-BoldEx.otf", 18)
+
+blockSurf = pygame.image.load("block.png")
+blockSurfSel = pygame.image.load("selblock.png")
+blockSurfBlank = pygame.image.load("blankblock.png")
+
+warn1 = pygame.image.load("block1.png")
+warn2 = pygame.image.load("block2.png")
+warn3 = pygame.image.load("block3.png")
+warn4 = pygame.image.load("block4.png")
+warn5 = pygame.image.load("block5.png")
+warn6 = pygame.image.load("block6.png")
+warn7 = pygame.image.load("block7.png")
+warn8 = pygame.image.load("block8.png")
+explode = pygame.image.load("explode.png")
+question = pygame.image.load("question.png")
+
+clock = pygame.image.load("time.png")
+
+
+class Box():
+    # flag = -3 # -3 = NO FLAG; -2 = FLAGGED; -1 = ?; 0 = BLANK; 1-8 = WARN; 9 = Exploded
+    isBomb = False
+    index = 0
+    x = 0
+    y = 0
+
+    def __init__(self, box_pos):
+        self.box_pos = box_pos
+        self.flag = -3
+
+
+refBlock = Box(Rect(-100, -100, 0, 0))
+refBlock.x = -10
+refBlock.y = -10
+selBlock = refBlock
+
+
+def draw_box():
+    pygame.draw.line(surfObj, black, (10, 10), (163, 10))
+    pygame.draw.line(surfObj, black, (10, 10), (10, 163))
+    pygame.draw.line(surfObj, black, (10, 163), (163, 163))
+    pygame.draw.line(surfObj, black, (163, 10), (163, 163))
+
+
+def draw_boxes_init(obj):
+    for x in range(0, 9):
+        for y in range(0, 9):
+            bo = Box(Rect(x*17+10, y*17+10, 17, 17))
+            bo.index = len(obj[0])
+            bo.x = x
+            bo.y = y
+            obj[0].append(bo)
+            surfObj.blit(blockSurf, (x*17 + 10, y*17 + 10))
+
+
+def pick_bombs(obj):
+    num_bombs = 0
+    while num_bombs < 10:   # CHange to 10
+        x = randrange(0, len(obj[0]))
+        if not obj[0][x].isBomb:
+            obj[0][x].isBomb = True
+            num_bombs += 1
+
+
+def draw_boxes():
+    for obj in OBJECTS:
+        obj[1] = 0
+        for b in obj[0]:
+            if b.flag == -3:
+                obj[1] += 1
+                surfObj.blit(blockSurf, b.box_pos)
+            elif b.flag == 0:
+                if not b in obj[2]:
+                    path_find(obj, b)
+                surfObj.blit(blockSurfBlank, b.box_pos)
+            elif b.flag == 9:
+                surfObj.blit(explode, b.box_pos)
+                lose(obj)
+            elif b.flag == -1:
+                surfObj.blit(question, b.box_pos)
+            elif b.flag == 1:
+                surfObj.blit(warn1, b.box_pos)
+            elif b.flag == 2:
+                surfObj.blit(warn2, b.box_pos)
+            elif b.flag == 3:
+                surfObj.blit(warn3, b.box_pos)
+            elif b.flag == 4:
+                surfObj.blit(warn4, b.box_pos)
+            elif b.flag == 5:
+                surfObj.blit(warn5, b.box_pos)
+            elif b.flag == 6:
+                surfObj.blit(warn6, b.box_pos)
+            elif b.flag == 7:
+                surfObj.blit(warn7, b.box_pos)
+            elif b.flag == 8:
+                surfObj.blit(warn8, b.box_pos)
+        if obj[1] == 10:
+            win(obj)
+
+
+def box_at(boxes, x, y):
+    for b in boxes:
+        if b.x == x and b.y == y:
+            return b
+
+
+def warnInteger(boxes, b):
+    try:
+        if b.x < 9 and b.x > 0 and b.y > 0 and b.y < 9:
+            return (box_at(boxes, b.x - 1, b.y), box_at(boxes, b.x + 1, b.y), box_at(boxes, b.x, b.y - 1), box_at(boxes, b.x, b.y + 1), box_at(boxes, b.x - 1, b.y - 1), box_at(boxes, b.x + 1, b.y + 1), box_at(boxes, b.x + 1, b.y - 1), box_at(boxes, b.x - 1, b.y + 1))
+        elif b.x > 8 and b.y > 0 and b.y < 9:
+            return (box_at(boxes, b.x - 1, b.y), box_at(boxes, b.x - 1, b.y - 1), box_at(boxes, b.x - 1, b.y + 1))
+        elif b.x < 1 and b.y > 0 and b.y < 9:
+            return (box_at(boxes, b.x + 1, b.y), box_at(boxes, b.x + 1, b.y - 1), box_at(boxes, b.x + 1, b.y + 1), box_at(boxes, b.x, b.y+1), box_at(boxes, b.x, b.y-1))
+        elif b.y < 1 and b.x > 0 and b.x < 9:
+            return (box_at(boxes, b.x, b.y + 1), box_at(boxes, b.x + 1, b.y + 1), box_at(boxes, b.x-1, b.y), box_at(boxes, b.x+1, b.y), box_at(boxes, b.x-1, b.y+1))
+        elif b.y == 0 and b.x == 0:
+            return (box_at(boxes, 1, 0), box_at(boxes, 1, 1), box_at(boxes, 0, 1))
+    except Exception as msg:
+        return None
+
+
+def get_warn(boxes, b):
+    warn = 0
+    try:
+        neighbors = warnInteger(boxes, b)
+        if not neighbors:
+            return 0
+        for b in neighbors:
+            try:
+                if b and b.isBomb:
+                    warn = warn + 1
+            except Exception as e:
+                print("Couldn't detect if neighbor is bomb..." + str(e))
+        return warn
+    except Exception as ee:
+        print("Couldn't find warn int..." + str(ee))
+        return warn
+
+
+boxesToPath = []
+
+
+def crossFind(boxes, b):
+    if b.x == 0 and b.y == 0:
+        return (box_at(boxes, 0, 1), box_at(boxes, 1, 0))
+    elif b.x == 0 and b.y == 9:
+        return (box_at(boxes, 0, 8), box_at(boxes, 1, 9))
+    elif b.x == 9 and b.y == 0:
+        return (box_at(boxes, 8, 0), box_at(boxes, 9, 1))
+    elif b.x == 9 and b.y == 9:
+        return (box_at(boxes, 8, 9), box_at(boxes, 9, 8))
+    elif b.x > 0 and b.x < 9 and b.y > 0:
+        return (box_at(boxes, b.x, b.y-1), box_at(boxes, b.x-1, b.y), box_at(boxes, b.x+1, b.y), box_at(boxes, b.x, b.y+1))
+    elif b.x == 0 and b.y > 0 and b.y < 9:
+        return (box_at(boxes, b.x, b.y-1), box_at(boxes, b.x, b.y+1), box_at(boxes, b.x+1, b.y))
+    elif b.x == 9 and b.y > 0 and b.y < 9:
+        return (box_at(boxes, b.x-1, b.y), box_at(boxes, b.x, b.y-1), box_at(boxes, b.x, b.y+1))
+    elif b.y == 0 and b.x > 0 and b.x < 9:
+        return (box_at(boxes, b.x - 1, b.y), box_at(boxes, b.x+1, b.y), box_at(boxes, b.x, b.y+1))
+    elif b.y == 9 and b.x > 0 and b.x < 9:
+        return (box_at(boxes, b.x - 1, b.y), box_at(boxes, b.x+1, b.y), box_at(boxes, b.x, b.y-1))
+
+
+def path_find(obj, b):
+    for bo in crossFind(obj[0], b):
+        if bo and get_warn(obj[0], bo) == 0:
+            bo.flag = 0
+    for bo2 in warnInteger(obj[0], b):
+        w = get_warn(obj[0], bo2)
+        if w > 0 and w < 9:
+            bo2.flag = w
+    obj[2].append(b)
+
+
+def lose(obj):
+    #message_box("Sorry, you lost!", "You lose!  " + str(OBJECTS.index(obj)), 0)
+    obj[3] += (81 - 10 - obj[1]) / GAME_SPACER  # fitness
+    obj[4] += 1  # gamecount
+    resetGame(obj)
+
+
+def win(obj):
+    message_box("Congrats, you won!", "You win!  " + str(obj[1]), 0)
+    obj[3] += (81 - 10 - obj[1]) / GAME_SPACER  # fitness
+    obj[4] += 1
+    resetGame(obj)
+
+
+def resetGame(obj):
+    obj[0][:] = []
+    obj[1] = 0
+    obj[2] = []
+    obj[6] = 0
+    draw_boxes_init(obj)
+    pick_bombs(obj)
+
+
+for objec in OBJECTS:
+    resetGame(objec)
+
+while True:
+    surfObj.fill(white)
+
+    draw_box()
+    draw_boxes()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    for index, objec in enumerate(OBJECTS):
+        if not objec[4] == 5:
+            boxes1d = []
+            for box_elem in objec[0]:
+                boxes1d.append(box_elem.flag)
+
+            # process
+
+            output = feedforward(objec[5], boxes1d)[0]
+            output *= 81
+            coords = [int(output % 9), int(output // 9)]
+            objec[6] += 1
+            if objec[6] == 81 - 20:
+                lose(objec)
+
+            #print(index, ": ", obj[1], ", ", coords)
+
+            for box_elem in objec[0]:
+                if [box_elem.x, box_elem.y] == coords:
+                    selBlock = box_elem
+                    break
+
+            selBlock.flag = get_warn(objec[0], selBlock)
+            if selBlock.flag == 0:
+                boxesToPath.append(selBlock)
+                path_find(objec, selBlock)
+            if selBlock.isBomb:
+                selBlock.flag = 9
+
+            if selBlock.flag == -3:
+                surfObj.blit(blockSurfSel, selBlock.box_pos)
+
+    if all([obj[4] == GAME_SPACER for obj in OBJECTS]):
+        CYCLE_COUNTER += 1
+        print("MUTATE_--------------------")
+        OBJECTS = get_mutations(SURVIVOR_COUNT)
+        for objec in OBJECTS:
+            resetGame(objec)
+
+    pygame.display.update()
+    # pygame.time.Clock().tick(30)
